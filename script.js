41996d406f5f5a2d4560c851b6c61e07ed25efba
dandeboer <ddeboer926@gmail.com>
2020-02-09 20:01:26 -0500

    delete button works, but I broke the add note buttongit add .git add .git add .

--- script.js
+++ script.js
@@ -7,17 +7,28 @@ let submit = document.querySelector(".submit")
 getNotes()
 
 function getNotes() {
-fetch("http://localhost:3000/notes/")
-    .then(response => response.json())
-    .then(function(data) {
-        let notesData = data
-        for (let noteData of notesData) {
-        createNote()
-        let title = document.querySelector(".title")
-        let body = document.querySelector(".body")
-        title.innerText = noteData.title
-        body.innerText = noteData.body
-    }})
+    fetch("http://localhost:3000/notes/")
+        .then(response => response.json())
+        .then(function (data) {
+            let notesData = data
+            for (let noteData of notesData) {
+                createNote()
+                let title = document.querySelector(".title")
+                let body = document.querySelector(".body")
+                let note = document.querySelector(".note")
+                title.innerText = noteData.title
+                body.innerText = noteData.body
+                let deleteButtons = document.querySelector(".delete")
+                deleteButtons.addEventListener("click", e => {
+                    note.remove()
+                    console.log(noteData.id + " deleted")
+                    fetch("http://localhost:3000/notes/" + noteData.id, {
+                        method: "delete",
+                        headers: { "Content-Type": "application/json" },
+                    })
+                })
+            }
+        })
 }
 
 function createNote() {
@@ -32,18 +43,22 @@ function createNote() {
     div.appendChild(p)
     p.setAttribute("class", "body")
     div.appendChild(button)
+    button.setAttribute("type", "button")
+    button.setAttribute("class", "delete")
+    button.innerText = "Delete"
+
 }
 
 form.addEventListener("submit", e => {
-e.preventDefault()
-let noteText = {body: bodyInput.value, title: titleInput.value}
-console.log(noteText)
-fetch("http://localhost:3000/notes/", {
-    method: "post",
-    headers: {"Content-Type": "application/json"}, 
-    body: JSON.stringify(noteText)
-})
-    .then(response => response.json())
+    e.preventDefault()
+    let noteText = { body: bodyInput.value, title: titleInput.value }
+    console.log(noteText)
+    fetch("http://localhost:3000/notes/", {
+        method: "post",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(noteText)
+    })
+        .then(response => response.json())
     titleInput.value = ""
     bodyInput.value = ""
 })
